2c4673cb0272f9981dce2720336df1ddd2a6a45d
Matias.Rosenberg <matias@prodytec.net>
2021-09-17 15:09:22 -0300

    Series principio

--- Cargar_series/Series.cs
+++ Cargar_series/Series.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Formulas;
 
 namespace Cargar_series
 {
@@ -16,12 +17,18 @@ namespace Cargar_series
         {
             InitializeComponent();
         }
-
+        Datagrid Datagrid = new Datagrid();
         private void Series_Load(object sender, EventArgs e)
         {
             cbfiltro.Items.Add("Hoy");
             cbfiltro.Items.Add("Mes actual");
             cbfiltro.Items.Add("Personalizado");
+
+            
+            //nombre del store para llenar el datagrid
+            string store = "sp_Ecommercebrm";
+
+            Datagrid.Llenardatagrid(dgvfacturas, store);
         }
     }
 }
---
+++ Formulas/Datagrid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulas
+{
+    public class Datagrid
+    {
+        SqlConnection cnn = Conexionbd.DbConnection.getDBConnection();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        public void Llenardatagrid(DataGridView dgv, string Consulta)
+        {
+            try
+            {
+
+                da = new SqlDataAdapter(Consulta, cnn);
+                dt = new DataTable();
+                da.Fill(dt);
+                dgv.DataSource = dt;
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo llenar la tabla" + ex.ToString());
+            }
+        }
+    }
+}
