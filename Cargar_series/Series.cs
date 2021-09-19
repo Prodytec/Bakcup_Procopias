c4213529dd7072f63e96705656be2c3cca5e7bab
Matias.Rosenberg <matias@prodytec.net>
2021-09-18 21:44:59 -0300

    Series casi cargadas

--- Cargar_series/Series.cs
+++ Cargar_series/Series.cs
@@ -25,20 +25,29 @@ namespace Cargar_series
             cbfiltro.Items.Add("Personalizado");
             cbfiltro.SelectedItem = cbfiltro.Items[0];
 
-            //nombre del store para llenar el datagrid
-            string store = "sp_Ecommercebrm";
-
-            Datagrid.Llenardatagrid(dgvfacturas, store);
         }
+
+        string Mes = "SP_fACTURAS_MES_ACTUAL";
+        string Hoy = "SP_FACTURAS_HOY";
 
         private void cbfiltro_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (cbfiltro.SelectedIndex == 0 || cbfiltro.SelectedIndex == 1)
+            if (cbfiltro.SelectedIndex == 0 )
             {
                 lbldesde.Visible = false;
                 lblhasta.Visible = false;
                 txtdesde.Visible = false;
                 txthasta.Visible = false;
+                Datagrid.Llenardatagrid(dgvfacturas, Mes);
+                
+            }
+            else if(cbfiltro.SelectedIndex == 1)
+            {
+                lbldesde.Visible = false;
+                lblhasta.Visible = false;
+                txtdesde.Visible = false;
+                txthasta.Visible = false;
+                Datagrid.Llenardatagrid(dgvfacturas, Hoy);
             }
             else
             {
